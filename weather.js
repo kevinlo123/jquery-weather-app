3d7f64f1d4163b9aa1a813fcfc38a7f2ef9e1843
Your Name <you@example.com>
2017-10-18 22:36:37 -0400

    added more functionality got main data pulled from api

--- weather.js
+++ weather.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
   $("#getWeather_Btn").on("click" , function(){
       var city = $("#inputCity").val();
+      $("#inputCity").val("");
       var apiCall = "https://api.openweathermap.org/data/2.5/weather?q="+ city +"&units=imperial&appid=cd63f1a3797d35b613e676b91131bf3b";
 
       $.getJSON(apiCall,weatherCall);
@@ -11,12 +12,28 @@ $(document).ready(function(){
         var cityTemp = (weatherData.main.temp);
         var cityTempMin = (weatherData.main.temp_min);
         var cityTempMax = (weatherData.main.temp_max);
+        var pressure = (weatherData.main.pressure); 
+        var humidity = (weatherData.main.humidity);                
         var description = (weatherData.weather[0].description);
+        var longitude = (weatherData.coord.lon);
+        var latitude = (weatherData.coord.lat);        
+        var icon = ("<img src='http://openweathermap.org/img/w/" + weatherData.weather[0].icon + ".png'>");        
         var country = (weatherData.sys.country);
         var wind = (weatherData.wind.speed);
         console.log(weatherData);
+        $("p").fadeIn(850);
         $("p").html("");
-        $("p").append("the weather for " + cityName + " is " + cityTemp);
+        $(".temperature").append("The weather in " + cityName + " " + country + "," + " is " + cityTemp + "&#8457;");
+        $(".description").append(description + icon);
+        $(".minMax").append("Min: " + cityTempMin + "&#8457;" + " Max: " + cityTempMax + "&#8457;");  
+        $(".pressure").append("Pressure: " + pressure + " hpa");
+        $(".humidity").append("Humidity: " + humidity + "%")              
+        $(".cord").append("Geo Coordinates: "+ "Longitude: " + longitude  + " Latitude: " + latitude);
       }
    });
+  $("#getWeather_Btn").on("click" , function(){ /*on click function for the fading in of another city user searches didnt work without it*/
+    $("p").fadeOut(150);
+  });
 });
+
+
